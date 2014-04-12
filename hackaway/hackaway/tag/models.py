3b4f712974e9d4dd67b69edc82f30b039d65f5f9
Ana Hristova <ana@fundedbyme.com>
2014-04-12 23:34:33 +0200

    Rename Tag model field

--- hackaway/hackaway/tag/models.py
+++ hackaway/hackaway/tag/models.py
@@ -4,5 +4,5 @@ from .constants import LANGUAGES
 
 
 class Tag(models.Model):
-    tag_name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
     language = models.CharField(max_length=2, choices=LANGUAGES, default='en')
