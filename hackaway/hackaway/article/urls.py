d89298cd0ba5793d443470a40d90fca7dedd22ad
Ana Hristova <ana@fundedbyme.com>
2014-04-13 11:14:02 +0200

    List the articles

--- hackaway/hackaway/article/urls.py
+++ hackaway/hackaway/article/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import patterns, url
 
-from .views import OmniArticlesView
+from .views import OmniArticlesView, PaginatedArticleListView
 
 urlpatterns = patterns(
     '',
     url(r'^omni/$',
         OmniArticlesView.as_view(),
         name="omni_articles"),
+    url(r'^page(?P<page>[0-9]+)/$',
+        PaginatedArticleListView.as_view(),
+        name="articles"),
+    url(r'^$',
+        PaginatedArticleListView.as_view(),
+        name="articles"),
 )
